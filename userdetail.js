e386d625bbaf075e75b8c377b1ce24f01316c085
veeqed <veeqed@gmail.com>
2024-08-25 15:42:41 +0700

    test

---
+++ userdetail.js
@@ -0,0 +1,6 @@
+<h1>Hello Vee San</h1>
+
+<script runat="server">
+	Platform.Load("core", "1");
+	Write('<input type="text" name="FirstName" placeholder="Your FirstName..." value="'+FirstName+'"/>');
+</script>
