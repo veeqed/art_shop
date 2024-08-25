a351bacec5aa14474c00f6c21bb601a2b1b281fc
veeqed <veeqed@gmail.com>
2024-08-25 15:48:07 +0700

    test3

--- userdetail.js
+++ userdetail.js
@@ -1,6 +1,6 @@
 <h1>Hello Vee San</h1>
 
-<script runat="server">
-	Platform.Load("core", "1");
-	Write('<input type="text" name="FirstName" placeholder="Your FirstName..." value="'+firstName+'"/>');
+<script>
+  var data = %%=v(@dataString)=%%; 
+  alert(data);
 </script>
